f1ca9cbed6d94ae2b33ef8ffbbc10a7807a4f220
Ivan Juarez <ivanjuarez99@hotmail.com>
2025-04-02 19:04:15 -0300

    Server Django con templates

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from tienda.views import productos
-from tienda.views import arreglos
 from django.contrib import admin
 
 urlpatterns = [
--- tienda/views.py
+++ tienda/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def productos(request):
-    return HttpResponse("Hola, este es el listado de productos test2")
-
+    #return HttpResponse("Hola, este es el listado de productos test2")
+    return render(request, "tienda/productos.html")
 
 def arreglos(request):
-    return HttpResponse("Hola, este es el listado de arreglos test2")
+    #return HttpResponse("Hola, este es el listado de arreglos test2")
+    return render(request, "tienda/arreglos.html")
